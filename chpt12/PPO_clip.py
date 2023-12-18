286cca62ab49ec714d316c966d9aed016de28e9c
mikemyzhao <tjzhaomengyi@163.com>
2023-12-18 11:17:22 +0800

    PPO算法
    PPO算法是在TRPO算法复杂计算过程问题上的改进，实现更加简单，这里例子代码使用PPO截断的算法进行实现。

--- chpt12/PPO_clip.py
+++ chpt12/PPO_clip.py
@@ -10,20 +10,16 @@ from tqdm import tqdm
 import chpt7.rl_utils as rl_utils
 import copy
 
-'倒立摆环境试验，它是与连续动作交互的环境，对于策略网络，因为环境是连续动作，所以策略网路哦分别输出表示动作分布的高斯分布的均值和方差'
-
-class PolicyNetContinuous(torch.nn.Module):
+"受限定义策略网络PoliycNet，输入是某个状态，输出是该状态下的动作概率分布，这里采样在离散动作空间上的softmax()函数来实现一个科学系的多项分布"
+class PolicyNet(torch.nn.Module):
     def __init__(self, state_dim, hidden_dim, action_dim):
-        super(PolicyNetContinuous, self).__init__()
+        super(PolicyNet, self).__init__()
         self.fc1 =torch.nn.Linear(state_dim, hidden_dim)
-        self.fc_mu = torch.nn.Linear(hidden_dim, action_dim)
-        self.fc_std = torch.nn.Linear(hidden_dim, action_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        mu = 2.0 * torch.tanh(self.fc_mu(x))
-        std = F.softplus(self.fc_std(x))
-        return mu,std
+        return F.softmax(self.fc2(x), dim=1)
 
 #AC网络多一个价值网络
 class ValueNet(torch.nn.Module):
@@ -36,31 +32,28 @@ class ValueNet(torch.nn.Module):
         x = F.relu(self.fc1(x))
         return self.fc2(x)
 
-
-
-class PPOContinous:
+class PPO:
     def __init__(self, state_dim, hidden_dim, action_dim, actor_lr, critic_lr, lmbda, epochs, eps, gamma, device):
-        #策略网络参数不需要优化更新器
-        #策略网络
-        self.actor = PolicyNetContinuous(state_dim, hidden_dim, action_dim).to(device)
-        #价值网络
+        # 策略网络参数不需要优化更新器
+        # 策略网络
+        self.actor = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
+        # 价值网络
         self.critic = ValueNet(state_dim, hidden_dim).to(device)
-        #网络优化器
+        # 网络优化器
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=actor_lr)
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=critic_lr)#价值网络优化器
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=critic_lr)  # 价值网络优化器
         self.gamma = gamma
-        self.lmbda = lmbda #GAE参数
-        self.epochs = epochs
-        self.eps = eps
+        self.lmbda = lmbda  # GAE参数
+        self.epochs = epochs #一条序列的数据用来训练轮数
+        self.eps = eps #PPO中截断范围的参数
         self.device = device
 
     def take_action(self, state):
         state = torch.tensor([state], dtype=torch.float).to(self.device)
-        mu, std = self.actor(state)
-        # probs = self.actor(state)
-        action_dist = torch.distributions.Normal(mu, std) #这个是连续的
+        probs = self.actor(state)
+        action_dist = torch.distributions.Categorical(probs)
         action = action_dist.sample()
-        return [action.item()]
+        return action.item()
 
     def update(self, transition_dict):
         states = torch.tensor(transition_dict['states'], dtype=torch.float).to(self.device)
@@ -68,61 +61,56 @@ class PPOContinous:
         rewards = torch.tensor(transition_dict['rewards'], dtype=torch.float).view(-1, 1).to(self.device)
         next_states = torch.tensor(transition_dict['next_states'], dtype=torch.float).to(self.device)
         dones = torch.tensor(transition_dict['dones'], dtype=torch.float).view(-1, 1).to(self.device)
-        rewards = (rewards + 8.0) / 8.0 #对奖励进行修改，方便训练
         td_target = rewards + self.gamma * self.critic(next_states) * (1 - dones)
         td_delta = td_target - self.critic(states)
         advantage = rl_utils.compute_advantage(self.gamma, self.lmbda, td_delta.cpu()).to(self.device)
-        mu, std = self.actor(states)
-        action_dists = torch.distributions.Normal(mu.detach(), std.detach())
-        old_log_probs = action_dists.log_prob(actions)
-
+        old_log_probs = torch.log(self.actor(states).gather(1, actions)).detach()
         for _ in range(self.epochs):
-            mu, std = self.actor(states)
-            action_dists = torch.distributions.Normal(mu, std)
-            log_probs = action_dists.log_prob(actions)
+            log_probs = torch.log(self.actor(states).gather(1, actions))
             ratio = torch.exp(log_probs - old_log_probs)
             surr1 = ratio * advantage
-            surr2 = torch.clamp(ratio, 1 - self.eps, 1 + self.eps) * advantage
-            actor_loss = torch.mean(-torch.min(surr1, surr2))
+            surr2 = torch.clamp(ratio, 1 - self.eps, 1 + self.eps) #advantage优势函数的截断
+            actor_loss = torch.mean(-torch.min(surr1, surr2)) #PPO损失函数
             critic_loss = torch.mean(F.mse_loss(self.critic(states), td_target.detach()))
             self.actor_optimizer.zero_grad()
             self.critic_optimizer.zero_grad()
             actor_loss.backward()
             critic_loss.backward()
             self.actor_optimizer.step()
-            self.critic_optimizer.step() #更新价值函数
+            self.critic_optimizer.step()
+
 
 
 #在车杆环境中训练TRPO
-num_episodes = 2000
+actor_lr = 1e-3
+critic_lr = 1e-2
+num_episodes = 500
 hidden_dim = 128
-gamma = 0.9
-lmbda = 0.9
-actor_lr = 1e-4
-critic_lr = 5e-3
+gamma = 0.98
+lmbda = 0.95
 epochs = 10
 eps = 0.2
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-env_name = "Pendulum-v0"
+env_name = "CartPole-v0"
 env = gym.make(env_name)
 env.seed(0)
 torch.manual_seed(0)
 state_dim = env.observation_space.shape[0]
-action_dim = env.action_space.shape[0]
-agent = PPOContinous(state_dim, hidden_dim, action_dim, actor_lr, critic_lr, lmbda, epochs, eps, gamma, device)
+action_dim = env.action_space.n
+agent = PPO(state_dim, hidden_dim, action_dim, actor_lr, critic_lr, lmbda, epochs, eps, gamma, device)
 return_list = rl_utils.train_on_policy_agent(env, agent, num_episodes)
 
 episodes_list = list(range(len(return_list)))
 plt.plot(episodes_list, return_list)
 plt.xlabel('Episodes')
 plt.ylabel('Returns')
-plt.title('PPO Continuous on {}'.format(env_name))
+plt.title('PPO_clip on {}'.format(env_name))
 plt.show()
 
-mv_return = rl_utils.moving_average(return_list, 21)
+mv_return = rl_utils.moving_average(return_list, 9)
 plt.plot(episodes_list, mv_return)
 plt.xlabel('Episodes')
 plt.ylabel('Returns')
-plt.title('PPO Continuous on {}'.format(env_name))
+plt.title('PPO_clip on {}'.format(env_name))
 plt.show()
